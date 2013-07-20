8bb916a92f3b6729b3c1fb28aba1cd091b82a70c
HolyCow <mike.beasterfeld@gmail.com>
2013-07-20 13:15:45 -0500

    Update seenbot to use a database

--- lib/seenbot.rb
+++ lib/seenbot.rb
@@ -1,38 +1,63 @@
+require 'date'
 require 'cinch'
+require 'data_mapper'
+require 'dm-migrations'
 
-class Seen < Struct.new(:who, :where, :what, :time)
-  def to_s
-    "[#{time}] #{who} was seen in #{where} saying '#{what}'"
-  end
+DataMapper::Logger.new($stdout, :debug)
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://vagrant:vagrant@localhost/5by5seenbot')
+
+class Users
+  include DataMapper::Resource
+
+  property :id,           Serial    # An auto-increment integer key
+  property :user,         String    
+  property :channel,      String    # A varchar type string, for short strings
+  property :message,      Text      # A text block, for longer string data.
+  property :updated_at,   DateTime  # A DateTime, for any date you might like.
 end
+
+DataMapper.finalize
+
+DataMapper.auto_upgrade!
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.server   = 'irc.freenode.org'
     c.channels = ["#5by5", "#5by5bottest"]
     c.nick = 'haveyouseenbot'
-
-    @users = {}
   end
 
   # Only log channel messages
   on :channel do |m|
-    if @users.nil?
-       @users = {}
-       puts( 'Initializing users hash' )
-    end
-    @users[m.user.nick] = Seen.new(m.user.nick, m.channel, m.message, m.time.to_s)
+    user = Users.first_or_create(:user => m.user.nick.downcase)
+
+    user.attributes = {
+      :channel => m.channel,
+      :message => m.message,
+      :updated_at => m.time
+    }
+
+    user.save
   end
 
-  on :channel, /^!seen (.+)/ do |m, nick|
+  on :private, /^!help/ do |m|
+    m.reply "Hello, #{m.user.nick}.  I am a bot that tracks the last time a user said something.  To check for a user, issue the command '!seen <username>'.  I was created by _HolyCow (aka HolyCow) and my source code can be found at http://github.com/holycow/5by5seenbot."
+  end
+
+  on :message, /^!seen ([^ ]+)/ do |m, nick|
     if nick.downcase == bot.nick.downcase
       m.reply "That's my name dude!"
     elsif nick.downcase == m.user.nick.downcase
       m.reply "That's your name dude!"
-    elsif @users.key?(nick.downcase)
-      m.reply @users[nick].to_s
     else
-      m.reply "I haven't seen #{nick}"
+      user = Users.first(:user => nick.downcase)
+
+      if user
+        m.reply "[#{user.updated_at.to_s}] #{nick} was seen in #{user.channel} saying '#{user.message}'"
+      else      
+        m.reply "I haven't seen #{nick}"
+      end
     end
   end
 end
